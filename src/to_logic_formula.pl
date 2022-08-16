f290644c91cdf510262cf9d9429d4c8bd96faf81
Hubert Badocha <badocha.hubert@gmail.com>
2022-08-16 20:40:34 +0200

    Removed redundant paren entity from logic formula

--- src/to_logic_formula.pl
+++ src/to_logic_formula.pl
@@ -1,4 +1,4 @@
 % to_logic_formula(+Clause, -Formula)
 % Converts grounded program to a logic formula describing Clause.
-% ~(x /\ y) \/ z = or(not(paren(and(x, y))), z)
+% ~(x /\ y) \/ z = or(not(and(x, y)), z)
 to_logic_formula(Clause, F).
