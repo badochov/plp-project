e9ad65d398ba66f50458cc4099291c7ba3b5d646
Hubert Badocha <badocha.hubert@gmail.com>
2022-08-17 23:52:33 +0200

    Improved calc_probability tests

--- src/calc_probability_test.pl
+++ src/calc_probability_test.pl
@@ -1,23 +1,20 @@
+:- begin_tests(calc_probability).
+
 :- [calc_probability].
 
 float_cmp(F1, F2) :- float_cmp(F1, F2, 0.001).
 float_cmp(F1, F2, Eps) :- F1 - Eps =< F2, F1 + Eps >= F2.
 
-test__calc_probability__1 :- 
+test(calc_probability__1) :- 
     calc_probability(node(leaf(1), 0.42, leaf(0)), P),
     float_cmp(P, 0.58).
 
-test__calc_probability__2 :- 
+test(calc_probability__2) :- 
     calc_probability(node(leaf(0), 0.42, leaf(1)), P),
     float_cmp(P, 0.42).
 
-test__calc_probability__3 :- 
-    calc_probability(node(node(leaf(1),0.42,node(leaf(1),0.42,leaf(0))),0.42,leaf(1)), P),
-    float_cmp(P, 0.897688).
+test(calc_probability__3) :- 
+    calc_probability(node(node(leaf(1),0.42,node(leaf(1),0.6,leaf(0))),0.9,leaf(1)), P),
+    float_cmp(P, 0.9748).
 
-
-test__calc_probability :-
-    test__calc_probability__1,
-    test__calc_probability__2,
-    test__calc_probability__3.
-    
+:- end_tests(calc_probability).
