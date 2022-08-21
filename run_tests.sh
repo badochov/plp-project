cec6366a8bba3c20ef4e967c0ea20cf1decee6ea
Vytautas Mickus <vmickus@gmx.com>
2022-08-21 12:09:19 +0200

    Make run_tests.sh only run *.plt tests.

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 if (( $# > 0 )); then
     swipl -g run_tests -t halt $1.plt
 else
-    for f in src/*.plt src/*_test.pl; do
+    for f in src/*.plt; do
         swipl -g run_tests -t halt $f
     done
 fi
