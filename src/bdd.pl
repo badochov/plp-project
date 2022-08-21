4ee2c9eac62e9cfa33d5feef064594f2760d7850
Vytautas Mickus <vmickus@gmx.com>
2022-08-21 12:49:28 +0200

    Move assign_to/4 simplify_formula_const/2 to formula.pl

--- src/bdd.pl
+++ src/bdd.pl
@@ -14,17 +14,6 @@ is_literal(false_).
 
 get_probability(Var, Ps, P) :- rb_lookup(Var, P, Ps).
 
-assign_to(and(F1, F2), V, A, and(Nf1, Nf2)) :-
-    assign_to(F1, V, A, Nf1),
-    assign_to(F2, V, A, Nf2), !.
-assign_to(or(F1, F2), V, A, or(Nf1, Nf2)) :-
-    assign_to(F1, V, A, Nf1),
-    assign_to(F2, V, A, Nf2), !.
-assign_to(not(F), V, A, not(Nf)) :-
-    assign_to(F, V, A, Nf), !.
-assign_to(V, V, A, A) :- !.
-assign_to(F, _, _, F) :- !.
-
 % construct_obdd(+Formula, +VariableOrdering, +VariableProbabilities, -OBDD)
 construct_obdd(F, [], Leaf) :- 
     simplify_formula(F, Sf),
@@ -45,32 +34,6 @@ construct_obdd(Formula, [V|T], OBDD) :-
             construct_obdd(Fs, T, OBDD)
         )
     )), !.
-
-% Simplifies formula removing falses and trues from the formula.
-simplify_formula(F, Sf) :- simplify_formula_const(F, Sf), !.
-simplify_formula(and(F1, F2), Sf) :- 
-    simplify_formula(F1, Sf1),
-    simplify_formula(F2, Sf2),
-    simplify_formula_const(and(Sf1, Sf2), Sf), !.
-simplify_formula(or(F1, F2), Sf) :- 
-    simplify_formula(F1, Sf1),
-    simplify_formula(F2, Sf2),
-    simplify_formula_const(or(Sf1, Sf2), Sf), !.
-simplify_formula(not(F), Sf) :-
-    simplify_formula(F, Sf1),
-    simplify_formula_const(not(Sf1), Sf), !.
-simplify_formula(F, F) :- !.
-
-simplify_formula_const(and(true_, F), Sf) :- simplify_formula(F, Sf), !.
-simplify_formula_const(and(F, true_), Sf) :- simplify_formula(F, Sf), !.
-simplify_formula_const(and(false_, _), false_) :- !.
-simplify_formula_const(and(_, false_), false_) :- !.
-simplify_formula_const(or(false_, F), Sf) :- simplify_formula(F, Sf), !.
-simplify_formula_const(or(F, false_), Sf) :- simplify_formula(F, Sf), !.
-simplify_formula_const(or(true_, _), true_) :- !.
-simplify_formula_const(or(_, true_), true_) :- !.
-simplify_formula_const(not(false_), true_) :- !.
-simplify_formula_const(not(true_), false_) :- !.
 
 % reduce_to_robdd(+OBDD, -ROBDD)
 reduce_to_robdd(OBDD, ROBDD) :- 
--- src/formula.pl
+++ src/formula.pl
@@ -129,3 +129,43 @@ evaluate_formula(TF_List, or(A,B), TF):-
     evaluate_formula(TF_List, A, TF1),
     evaluate_formula(TF_List, B, TF2),
     (((TF1 = true;TF2 = true), TF = true); TF = false), !.
+
+
+assign_to(and(F1, F2), V, A, and(Nf1, Nf2)) :-
+    assign_to(F1, V, A, Nf1),
+    assign_to(F2, V, A, Nf2), !.
+assign_to(or(F1, F2), V, A, or(Nf1, Nf2)) :-
+    assign_to(F1, V, A, Nf1),
+    assign_to(F2, V, A, Nf2), !.
+assign_to(not(F), V, A, not(Nf)) :-
+    assign_to(F, V, A, Nf), !.
+assign_to(V, V, A, A) :- !.
+assign_to(F, _, _, F) :- !.
+
+
+% Simplifies formula removing falses and trues from the formula.
+simplify_formula(F, Sf) :- simplify_formula_const(F, Sf), !.
+simplify_formula(and(F1, F2), Sf) :-
+    simplify_formula(F1, Sf1),
+    simplify_formula(F2, Sf2),
+    simplify_formula_const(and(Sf1, Sf2), Sf), !.
+simplify_formula(or(F1, F2), Sf) :-
+    simplify_formula(F1, Sf1),
+    simplify_formula(F2, Sf2),
+    simplify_formula_const(or(Sf1, Sf2), Sf), !.
+simplify_formula(not(F), Sf) :-
+    simplify_formula(F, Sf1),
+    simplify_formula_const(not(Sf1), Sf), !.
+simplify_formula(F, F) :- !.
+
+simplify_formula_const(and(true_, F), Sf) :- simplify_formula(F, Sf), !.
+simplify_formula_const(and(F, true_), Sf) :- simplify_formula(F, Sf), !.
+simplify_formula_const(and(false_, _), false_) :- !.
+simplify_formula_const(and(_, false_), false_) :- !.
+simplify_formula_const(or(false_, F), Sf) :- simplify_formula(F, Sf), !.
+simplify_formula_const(or(F, false_), Sf) :- simplify_formula(F, Sf), !.
+simplify_formula_const(or(true_, _), true_) :- !.
+simplify_formula_const(or(_, true_), true_) :- !.
+simplify_formula_const(not(false_), true_) :- !.
+simplify_formula_const(not(true_), false_) :- !.
+
