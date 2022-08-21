10d32f90d2c414c2e5bf400521db8fed7c64c324
Vytautas Mickus <vmickus@gmx.com>
2022-08-21 23:43:22 +0200

    Fix conditional probabilities.

--- src/probability.pl
+++ src/probability.pl
@@ -7,12 +7,23 @@ prob(Query, Evidence, Prob):-
     problog_collect(P),
     ground_program(P, GLs, GP),
     program_formula(GP, Pf), formula(Query, Qf), formula(Evidence, Ef),
-    reduce_and_product([Pf, Qf], F1),
-    formula_given_evidence(F1, Ef, F),
-    simplify_formula(F, Fs),
-    construct_bdd(Fs, BDD),
+    reduce_and_product([Pf, Qf, Ef], F1),
+    simplify_formula(F1, F1s), construct_bdd(F1s, BDD1),
+
+    reduce_and_product([Pf, Ef], F2),
+    simplify_formula(F2, F2s), construct_bdd(F2s, BDD2),
+
     grounded_literals_to_variable_probabilities_tree(GLs, VarPs),
-    calc_probability(BDD, VarPs, Prob), !.
+    calc_probability(BDD1, VarPs, ProbQ),
+    calc_probability(BDD2, VarPs, ProbE),
+    Prob is ProbQ / ProbE, !.
 
 prob(Query, Prob):-
-    prob(Query, true, Prob).
+    problog_collect(P),
+    ground_program(P, GLs, GP),
+    program_formula(GP, Pf), formula(Query, Qf),
+    reduce_and_product([Pf, Qf], F1),
+    simplify_formula(F1, F1s), construct_bdd(F1s, BDD1),
+
+    grounded_literals_to_variable_probabilities_tree(GLs, VarPs),
+    calc_probability(BDD1, VarPs, Prob), !.
